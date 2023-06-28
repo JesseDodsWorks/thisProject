575a75dfa357688117206b7eff28cbbe3654bb63
Jesse Dods <jessedodsworks@gmail.com>
2023-06-28 16:00:29 -0700

    emailer is ready

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -2,9 +2,10 @@
 from flask import render_template, redirect, request, session
 from flask_app.models.user import User
 from flask_app import app
-from flask_bcrypt import Bcrypt
-bcrypt = Bcrypt(app)
 
+from email.message import EmailMessage
+import ssl
+import smtplib
 
 #######################################################
 #                 loading start
@@ -12,4 +13,45 @@ bcrypt = Bcrypt(app)
 @app.route("/")
 def register_login():
     return render_template ("home.html")
+#######################################################
+
+
+#######################################################
+##                  emailer test
+#######################################################
+@app.route("/sendemail", methods=["POST"])
+def sendemail():
+    auto_mailer = "noreplybarleyandbrew@gmail.com"
+    receiver1 = "manager@barleyanddbrew.com"
+    receiver2 = "mike@barleyanddbrew.com"
+
+    with open("flask_app/controllers/test.txt", "r") as f:
+        password = f.read()
+
+    sender = request.form["email"]
+    subject = request.form["subject"]
+
+    description = f"""
+    Email: {sender}
+    Subject Type: {subject}
+    Description: {request.form["description"]}
+    """
+
+    msg = EmailMessage()
+    msg["From"] = sender
+    msg["subject"] = f"no-reply QCC Submission - {subject}"
+    msg.set_content(description)
+    
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as smtp:
+        smtp.login(auto_mailer, password)
+        smtp.sendmail(auto_mailer, receiver1, msg.as_string())
+        smtp.sendmail(sender, receiver2, msg.as_string())
+        smtp.quit()
+        print("message successful")
+
+    return redirect("/")
+
+
 #######################################################
--- flask_app/models/user.py
+++ flask_app/models/user.py
@@ -2,8 +2,7 @@ from flask_app.config.mysqlconnection import connectToMySQL
 from flask_app import app
 from flask import flash
 import re
-from flask_bcrypt import Bcrypt
-bcrypt = Bcrypt(app)
+
 
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9.+_-]+\.[a-zA-Z]+$')
 
@@ -136,3 +135,26 @@ class User:
 
         return is_valid
 #######################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
